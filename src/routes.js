b1e5617955ad1671d1e469b0c1d442b4aaf5f441
MinK <minkoo0619@gmail.com>
2019-08-05 21:38:16 -0300

    Primeira configuração de desafio final e Criação de UserController

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import User from './app/models/User';
+
+const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Min',
+    email: 'minkoo',
+    password_hash: '12312312',
+  });
+
+  return res.json(user);
+});
+
+export default routes;
